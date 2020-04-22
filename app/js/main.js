9e342707f683fcd26cdd84ad8f95535691871d95
andrii_lutsyshyn <lutsishin07@gmail.com>
2020-04-23 00:28:15 +0300

    Сверстано три секции. Добавлены миксины и переменные. Первый коммит на гит. Не сделан поворот стрелки при открытии списка в 3-й секции

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+$('.slider-items').slick({
+  dots: false,
+  infinite: true,
+  speed: 100,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+jQuery(function ($) {
+  $('.solution-item').click(function () {
+    $(this).toggleClass('active').next()[$(this).next().is(':hidden') ? "slideDown" : "slideUp"](400);
+  });
+
+});
