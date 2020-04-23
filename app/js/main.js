fa4a5b5ad23084255f35d7664f890386183dcca5
andrii_lutsyshyn <lutsishin07@gmail.com>
2020-04-23 14:34:39 +0300

    Сделан дизайн слайдера отзывов не полностью. Сделана форма.

--- app/js/main.js
+++ app/js/main.js
@@ -35,9 +35,43 @@ $('.slider-items').slick({
   ]
 });
 
-jQuery(function ($) {
+$(function ($) {
   $('.solution-item').click(function () {
     $(this).toggleClass('active').next()[$(this).next().is(':hidden') ? "slideDown" : "slideUp"](400);
   });
 
+});
+
+$('.slider-reviews').slick({
+  dots: false,
+  infinite: true,
+  speed: 100,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: $('.prev--reviews'),
+  nextArrow: $('.next--reviews'),
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
